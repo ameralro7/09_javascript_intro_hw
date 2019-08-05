c5e3336fe20f821134d8b8f37b144a3a54b21590
Muhammad alqhtani <ms.qh505@gmail.com>
2019-08-05 16:35:57 +0300

    HW submision for Mohammed saad
    HW submision for Mohammed saad

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Loaded!");
+var firstWord = "Please";
+var secondWord = "squeeze";
+var thirdWord = "the";
+var fourthWord = "cheese";
+var Loaded =firstWord+secondWord+thirdWord+fourthWord
+console.log("Loaded");
